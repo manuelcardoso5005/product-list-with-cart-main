389eee66a6acc54fb8156c53ffb8a3eec6ad5817
Manuel Cardoso <manuelcardosomasc@gmail.com>
2024-08-13 16:50:08 +0100

    estilizando

--- js/script.js
+++ js/script.js
@@ -16,17 +16,25 @@ fetch(api)
       const item = data[i];
       htmlContent += `
       <div class="card">
-          <div class="img"> 
+          <div class="img-food"> 
             <picture>
               <source media="(min-width: 1200px)" srcset="${item.image.desktop}">
               <source media="(min-width: 768px)" srcset="${item.image.tablet}">
               <img src="${item.image.mobile}" alt="${item.name}">
             </picture>
+            <div class="add-panel-content cont-cart">
+              <div class="add-panel hide">
+                <img src="assets/images/icon-add-to-cart.svg" alt="Add to Cart">
+                <span>Add to Cart</span>
+              </div>
+              <div class="quant-panel show">
+                <p class="signal less">-<p>  
+                <p>1<p>
+                <p class="signal plus">+<p>
+              </div>
+            </div>
           </div>
-          <div class="add-panel">
-            <img src="assets/images/icon-add-to-cart.svg" alt="">
-            <span>Add to Cart</span>
-          </div>
+
         <div class="food-content">
           <p class="food-type">${item.category}</p>
           <p class="food-name">${item.name}</p>
